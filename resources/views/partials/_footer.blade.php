7533b77a8761512ca54551a1bb91d7a845895b53
MIlyasKohistani <m.ilyas.kohistani@amiricollections.com>
2021-10-20 18:41:40 +0300

    website front-end template with master layout created

---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<footer >
+        <p class="font-weight-medium text-center text-small">
+          Free <a href="https://www.bootstrapdash.com/" target="_blank" class="text-dark">Bootstrap dashboard templates</a> from Bootstrapdash
+        </p>
+  
+    <!-- Copyright -->
+    <div >©  2021 - WAM Denim
+    </div>
+    <!-- Copyright -->
+  
+   </footer>
+   <!-- Footer -->
---
+++ resources/views/partials/_nav.blade.php
@@ -0,0 +1,24 @@
+<header>
+    <nav class="navbar navbar-expand-sm navbar-light bg-white">
+        <div class="container">
+            <a class="navbar-brand font-weight-bold" href="https://www.wamdenim.com/">WAM DENIM</a>
+            <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId"
+                aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+                <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.wamdenim.com/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.wamdenim.com/service/about/">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.wamdenim.com/service/klantenservice/">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
